6ef272cb7d17378d7ad4b46c9c22b9ed9d0389c5
Hasala2002 <hasala.h@hotmail.com>
2021-06-05 23:16:59 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,41 @@
 // credit: http://www.javascriptkit.com/javatutors/touchevents2.shtml
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('service worker registered'))
-      .catch(err => console.log('service worker not registered', err));
-  }
+// if('serviceWorker' in navigator){
+//     navigator.serviceWorker.register('/sw.js')
+//       .then(reg => console.log('service worker registered'))
+//       .catch(err => console.log('service worker not registered', err));
+//   }
 
+const navigationBtn = document.getElementById('nav')
+
+const dataSLactive = document.getElementById('data-sl-active')
+const dataSLrecover = document.getElementById('data-sl-recover')
+const dataSLdeaths = document.getElementById('data-sl-deaths')
+const dataSLconfirm = document.getElementById('data-sl-confirm')
+const dataSLrecoverrate = document.getElementById('data-sl-recover-rate')
+const dataSLdeathrate = document.getElementById('data-sl-death-rate')
+const dataWORactive = document.getElementById('data-world-active')
+const dataWORrecover = document.getElementById('data-world-recover')
+const dataWORdeaths = document.getElementById('data-world-deaths')
+const dataWORconfirm = document.getElementById('data-world-confirm')
+const dataWORrecoverrate = document.getElementById('data-world-recover-rate')
+const dataWORdeathrate = document.getElementById('data-world-death-rate')
+
+const dataSLtimer = document.getElementById('data-sl-timer')
+const dataWORtimer = document.getElementById('data-world-timer')
+
+var tCC = document.getElementById('totalCasesChart').getContext('2d');
+var oDC = document.getElementById('overallDonut').getContext('2d');
+
+const dataTable = document.getElementById('data-table')
+
+navigationBtn.addEventListener('click',()=>{
+ 
+    if (document.documentElement.getAttribute("data-theme")!=="dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  })
 
 function swipedetect(el, callback) {
     var touchsurface = el,
@@ -104,3 +135,150 @@ function swipedetect(el, callback) {
     getWor.setAttribute('class','button_selected')
     getSl.setAttribute('class','button_ns')
   })
+
+  var covidStatus = {}
+
+  fetch('https://hpb.health.gov.lk/api/get-current-statistical')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.data)
+    setCovidStatus(data.data)
+    setTableData(data.data.hospital_data)
+  });
+
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+  function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.textContent = numberWithCommas(Math.floor(progress * (end - start) + start));
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+  }
+  
+  // const obj = document.getElementById("value");
+  // animateValue(obj, 100, 0, 5000);
+
+
+  const setCovidStatus = (data) => {
+    const ANIMATION_SPEED = 1000
+    // dataSLactive.textContent = data.local_active_cases
+
+    const RESOLVED_CASES_SL = data.local_total_cases - data.local_active_cases
+    const RESOLVED_CASES_WOR = data.global_total_cases - (data.global_total_cases-data.global_recovered-data.global_deaths)
+
+    animateValue(dataSLactive,0,data.local_active_cases,ANIMATION_SPEED)
+    animateValue(dataSLrecover,0,data.local_recovered,ANIMATION_SPEED)
+    animateValue(dataSLdeaths,0,data.local_deaths,ANIMATION_SPEED)
+    animateValue(dataSLconfirm,0,data.local_total_cases,ANIMATION_SPEED)
+    animateValue(dataWORactive,0,(data.global_total_cases-data.global_recovered-data.global_deaths),ANIMATION_SPEED)
+    animateValue(dataWORrecover,0,data.global_recovered,ANIMATION_SPEED)
+    animateValue(dataWORdeaths,0,data.global_deaths,ANIMATION_SPEED)
+    animateValue(dataWORconfirm,0,data.global_total_cases,ANIMATION_SPEED)
+
+    dataSLtimer.innerHTML = `<span class="material-icons data-icon-timer">restore</span>last updated ${moment(data.update_date_time).fromNow()}`
+    dataWORtimer.innerHTML = `<span class="material-icons data-icon-timer">restore</span>last updated ${moment(data.update_date_time).fromNow()}`
+    // console.log(moment(data.update_date_time).fromNow())
+
+    dataSLrecoverrate.innerHTML = `(${(100-(data.local_deaths/RESOLVED_CASES_SL)*100).toFixed(2)}%)<span class="material-icons data-rate">info</span>`
+    dataSLdeathrate.innerHTML = `(${((data.local_deaths/RESOLVED_CASES_SL)*100).toFixed(2)}%)<span class="material-icons data-rate">info</span>`
+
+    dataWORrecoverrate.innerHTML = `(${(100-(data.global_deaths/RESOLVED_CASES_WOR)*100).toFixed(2)}%)<span class="material-icons data-rate">info</span>`
+    dataWORdeathrate.innerHTML = `(${((data.global_deaths/RESOLVED_CASES_WOR)*100).toFixed(2)}%)<span class="material-icons data-rate">info</span>`
+
+
+    var overallCases = new Chart(oDC, {
+      type: 'doughnut',
+      data: {
+          labels: ['Active Cases', 'Recovered Cases', 'Deaths' ],
+          datasets: [{
+              label: 'Current Statistics',
+              data: [data.local_active_cases,data.local_recovered,data.local_deaths],
+              backgroundColor: [
+                'rgba(112, 82, 251, 0.75)',
+                'rgba(80, 205, 138, 0.75)',
+                'rgba(227, 52, 47, 0.75'
+              ],
+              borderColor: [
+                'rgb(112, 82, 251)',
+                'rgb(80, 205, 138)',
+                'rgb(227, 52, 47)'
+              ],
+              hoverOffset: 4,
+              borderWidth: 2
+          }],
+          
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  }
+
+  fetch('https://api.covid19api.com/country/sri-lanka/status/confirmed')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    var totalCasesChart = new Chart(tCC, {
+      type: 'line',
+      data: {
+          labels: data.map(dat=>moment(dat.Date).format('MMM YY')),
+          datasets: [{
+              label: 'Total Cases',
+              data: data.map(dat=>dat.Cases),
+              fill: false,
+              borderColor: 'rgba(253, 176, 26, 1)',
+              tension: 0.1,
+              pointRadius: 0
+          }],
+          
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  });
+
+  const setTableData = (hospitals) =>{
+    hospitals.map(hospital=>{
+      let $dataSet = $("<div>", {"class": "table-data-row"})
+      let $hospitalName = $("<span>", {"class": "hospital-name"}).text(hospital.hospital.name)
+      let $countData = $("<div>", {"class": "count-data"})
+      let $countSL = $("<span>", {"class": "loc-data"}).text(hospital.treatment_local)
+      let $countFor = $("<span>", {"class": "for-data"}).text(hospital.treatment_foreign)
+      let $countTotal = $("<span>", {"class": "tot-data"}).text(hospital.treatment_total)
+      $countData.append($countSL)
+      $countData.append($countFor)
+      $countData.append($countTotal)
+      $dataSet.append($hospitalName)
+      $dataSet.append($countData)
+      $("#tableData").append($dataSet)
+    })
+  }
+
+  $('#new-tab').click(function(){
+    if($('#table-section').attr('class')==='table-section'){
+    $('#table-section').attr('class','table-section-hovering')
+    $('#new-tab').text('close')
+  }else{
+    $('#table-section').attr('class','table-section')
+    $('#new-tab').text('open_in_new')
+  }
+});
